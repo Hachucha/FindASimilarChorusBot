83f444807de33dc0ad19e51bcdd1a7818685f3cc
Hachucha <78805507+Hachucha@users.noreply.github.com>
2023-01-22 06:20:03 +0300

    Add files via upload

---
+++ chorus_selection_norhyme.js
@@ -0,0 +1,69 @@
+let fs = require('fs');
+ 
+
+let vovel_letters_big = ['E','Y','U','I','O','A','У','Е','Ы','А','О','Э','Я','И','Ю','Ё'];
+let vovel_letters_small = ['e','y','u','i','o','a','у','е','ы','а','о','э','я','и','ю','ё'];
+
+const  vovel_pattern = (inp) => {
+    var outp = [];
+    //console.log(inp);
+    for(const symb of inp){
+        if (vovel_letters_small.includes(symb) 
+        || vovel_letters_big.includes(symb) 
+        || symb=='\n' ){//|| symb=='\t' || symb==' '
+            outp += symb;
+        }
+        //else console.log(symb);
+    };
+    return outp;
+}
+
+const vovel_pattern_lines = (inp) => {
+    return vovel_pattern(inp).split('\n');
+}
+
+const search = (input) => {
+
+    //читаем базу треков
+    let songs = fs.readFileSync('./songs.json', 'utf8'); 
+    songs = JSON.parse(songs);
+
+    var maxRelevantSongRating = 0;//рейтинг - это количество баллов релевантности по моему алгоритму
+    var maxRelevantSong = {};
+    
+    var inputVovPattLines = vovel_pattern_lines(input);//паттерн гласных/слогов четверостишья
+    for (const song of songs){
+        var songVovPattLines = vovel_pattern_lines(song.chorus);//паттерн гласных/слогов текущей песни
+        var currentSongRating = 0; 
+
+
+        //определяем максимальное количество строк, в которое гарантированно укладываются и припев песни и введенное четверостишие
+        var lines_max_count;
+        if (inputVovPattLines.length>songVovPattLines.length)
+            lines_max_count = songVovPattLines.length;
+        else
+            lines_max_count = inputVovPattLines.length;
+
+        
+
+        //Сравниваем количество слогов в соответствующих строках 
+        for (let i=0; i<lines_max_count; i++){
+            currentLineRating = Math.sqrt(1  - Math.abs((inputVovPattLines[i].length -songVovPattLines[i].length )/inputVovPattLines[i].length ));
+
+            currentSongRating += currentLineRating;
+
+        }            
+
+        //если новая песня релевантнее прежней, то теперь она самая релевантной
+        if (currentSongRating>maxRelevantSongRating){
+            maxRelevantSong = song;
+        }
+
+    }
+
+    return maxRelevantSong;
+}
+
+module.exports = {
+    search
+}
---
+++ main.js
@@ -0,0 +1,50 @@
+const TelegramApi = require('node-telegram-bot-api')
+const { search } = require('./chorus_selection_norhyme')
+
+const token = '5454680334:AAEqIe9TTTGua40oE2FJKfmlw8HuZ_eQzeg'
+const bot = new TelegramApi(token, {polling: true})
+
+const responseSearch = async (chatId, text) => {
+    let song = search(text);
+
+    let songName;
+
+    if(Object.keys(song).length === 0){
+        bot.sendMessage("Извините, что-то пошло не так.");
+    }
+
+    if(song.name = ""){
+        songName = song.singer + " - " + song.name;
+    }
+    else{
+        songName = song.singerAndName;
+    }
+
+    bot.sendMessage(chatId, `Максимально подходящая песня из нашей базы: `+songName+".\nВот ее припев:\n\n"+song.chorus);
+}
+
+const start = async () => {
+
+    bot.setMyCommands([
+        {command: '/start', description: 'Начать использовать бота.'},
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        try {
+            if (text === '/start') {
+                return bot.sendMessage(chatId, `Привет. Этот бот может подобрать известную песню, на мотив припева которой можно напеть ваше стихотоворение.\n\nПримечание: чем короче отрывок стихотворения, тем правдивее совпадение. \n\nНапишите отрывок вашего стихотворения.`);
+            }
+            responseSearch(chatId, text); 
+        } catch (e) {
+            return bot.sendMessage(chatId, 'Произошла какая то ошибочка!)');
+        }
+
+    })
+
+   
+}
+
+start()
